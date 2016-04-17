1179f0bd71d51c7d6e95d27c8398a2a87ac7596f
Kolya <raloz1@gmail.com>
2016-04-17 18:14:52 +0400

    Add main logo and link to FB icon

--- application/views/elements/header.php
+++ application/views/elements/header.php
@@ -1,11 +1,18 @@
 <div class="header">
 	<div class="header-top clearfix hidden-xs">
-		<span class="fa fa-facebook"></span>
+		<a href="#" class="unstyled">
+			<span class="fa fa-facebook"></span>
+		</a>
 		<span class="pull-right">
 			<span class="fa fa-map-marker"></span>
 			<span class="bpg-excelsior-caps"><?php echo $address_1; ?></span>
 			<span class="fa fa-map-marker"></span>
 			<span class="bpg-excelsior-caps"><?php echo $address_2; ?></span>
 		</span>
-	</div>
-</div>
+	</div><!-- header-top -->
+	<div class="header-main clearfix">
+		<a href="<?php echo locale_url(); ?>">
+			<img class="logo" alt="Logo" src="<?php echo static_url().'img/logo.png'; ?>">
+		</a>
+	</div><!-- header-main -->
+</div><!-- /header -->

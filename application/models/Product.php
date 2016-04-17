51ce873eb0d6bb45c00622b37581698c305063d1
Kolya <raloz1@gmail.com>
2016-04-17 23:47:00 +0400

    Revert "Revert "Copy lang files in system, to avoid "language file not found" errors""
    This reverts commit 014bfebf3d4919c15c17fb985f75b23943f9161a.

---
+++ application/models/Product.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends MY_Model {
+
+	
+
+}
+
+/* End of file Product.php */
+/* Location: ./application/models/Product.php */

454bb0172d4534c04799f0ca895e683c0432f73b
Kolya <raloz1@gmail.com>
2016-04-27 03:18:34 +0400

    Added hover icons over latest products

--- application/views/elements/latest_products.php
+++ application/views/elements/latest_products.php
@@ -9,9 +9,21 @@
 					style="background-image: url('<?php echo $image; ?>');"></div>
 				<div class="name bpg-excelsior-caps">
 					<div class="price white text-center"><?php echo $p->price; ?>₾</div>
-					<div class="text-right"><?php echo $p->name; ?></div>
+					<?php if(mb_strlen($p->name) <= 17): ?>
+						<div class="text-right"><?php echo $p->name; ?></div>
+					<?php else: ?>
+						<?php $name = mb_substr($p->name, 0, 16).'...'; ?>
+						<div class="text-right"><?php echo $name; ?></div>
+					<?php endif; ?>
 				</div>
-				<div class="overlay"></div>
+				<div class="overlay text-center white">
+					<a class="unstyled" href="#">
+						<span class="fa fa-search"></span>
+					</a>
+					<a class="unstyled" href="#">
+						<span class="fa fa-cart-plus"></span>
+					</a>
+				</div>
 			</div>
 		</div>
 	<?php endforeach; ?>

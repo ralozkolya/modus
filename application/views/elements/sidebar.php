114e73c545699979905f2a8f5cf9d674c151a6b5
Kolya <raloz1@gmail.com>
2016-05-02 23:06:13 +0400

    Fixed padding-top on category grid

--- application/views/elements/sidebar.php
+++ application/views/elements/sidebar.php
@@ -1,8 +1,13 @@
 <ul class="sidebar-categories bpg-excelsior-caps">
 	<?php foreach($categories as $c): ?>
-		<li>
+		<li class="sidebar-category">
 			<?php echo $c->name; ?>
 			<span class="fa fa-chevron-down pull-right"></span>
+			<ul>
+				<li>dsa</li>
+				<li>dsa</li>
+				<li>dsa</li>
+			</ul>
 		</li>
 	<?php endforeach; ?>
 </ul>

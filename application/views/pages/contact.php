9776f5da2932f5b8770883a20a6373bfdca4f590
Kolya <raloz1@gmail.com>
2017-01-15 23:39:32 +0400

    Change map

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -13,13 +13,7 @@
 			<div class="container bpg-excelsior">
 				<div class="row">
 					<div class="col-sm-6">
-						<iframe
-							width="100%"
-							height="450"
-							frameborder="0" style="border:0"
-							src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDf-orhpFhP16iptHhYvSD8rXk1iD303AI
-							&q=Batumi,Georgia" allowfullscreen>
-						</iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1490.7879639532982!2d41.64693165834592!3d41.64329699481042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDM4JzM1LjkiTiA0McKwMzgnNTIuOSJF!5e0!3m2!1sen!2sge!4v1484509054073" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
 					<div class="col-sm-4 col-sm-offset-1">
 						<form>
